61afa0e19225a741c86e36e3e1a84353ce2117c9
Eric Greene <eric@t4d.io>
2020-07-27 11:53:57 -0400

    exercise 3

--- demo-app/src/components/CarTool.js
+++ demo-app/src/components/CarTool.js
@@ -2,10 +2,34 @@ import React from 'react';
 
 export const CarTool = () => {
 
-  return ( 
-    <header>
-      <h1>Car Tool</h1>
-    </header>
+  return (
+    <>
+      <header>
+        <h1>Car Tool</h1>
+      </header>
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Make</th>
+            <th>Model</th>
+            <th>Year</th>
+            <th>Color</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Ford</td>
+            <td>Fusion Hybrid</td>
+            <td>2018</td>
+            <td>white</td>
+            <td>45000</td>
+          </tr>
+        </tbody>
+      </table>
+    </>
   );
 
 };
--- demo-app/src/components/ColorTool.js
+++ demo-app/src/components/ColorTool.js
@@ -2,16 +2,37 @@ import React from 'react';
 
 export const ColorTool = () => {
 
+  const colors = [
+    { id: 1, name: 'green', hexcode: '00ff00' },
+    { id: 2, name: 'purple', hexcode: 'ff00ff' },
+    { id: 3, name: 'blue', hexcode: '0000ff' },
+    { id: 4, name: 'hot pink', hexcode: 'FF69B4' },
+  ];
+
+  // const colorListItems = [];
+
+  // imperative approach
+  // for (let x = 0; x<colors.length; x++) {
+  //   colorListItems.push(<li>{colors[x]}</li>);
+  // }
+
+  // declarative approach
+  // semantic meaning: forEach is cause a side effect
+  // colors.forEach(color => {
+  //   colorListItems.push(<li>{color}</li>);
+  // });
+
+  // declarative
+  // semantic meaning of map is to transform from one array to another array
+  // const colorListItems = colors.map(color => <li>{color}</li>);
+
   return (
     <>
       <header>
         <h1>Color Tool</h1>
       </header>
       <ul>
-        <li>green</li>
-        <li>purple</li>
-        <li>blue</li>
-        <li>hot pink</li>
+        {colors.map(color => <li key={color.id}>{color.name}</li>)}
       </ul>
     </>
   );
