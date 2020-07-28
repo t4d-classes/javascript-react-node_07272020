f018248f16a4cd6ac3a67faad6c2932a6f321735
Eric Greene <eric@t4d.io>
2020-07-28 11:58:28 -0400

    add color form

---
+++ demo-app/src/components/ColorForm.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+
+export const ColorForm = ({ buttonText }) => {
+
+  const [ colorForm, setColorForm ] = useState({
+    name: '', hexcode: '',
+  });
+  
+
+  const change = (e) => {
+
+    setColorForm({
+      // object spread operator
+      ...colorForm, // copy the original color form
+      [ e.target.name ]: e.target.value,
+    });
+
+  };
+
+  const submitColor = () => {
+
+  };
+
+  return (
+    <form>
+      <div>
+        {/* React.createElement('label', { htmlFor: 'color-name-input' }, 'Color Name') */}
+        <label htmlFor="color-name-input">Color Name</label>
+        <input type="text" id="color-name-input" name="name"
+          value={colorForm.name} onChange={change} />
+      </div>
+      <div>
+        <label htmlFor="color-hexcode-input">Color Hexcode</label>
+        <input type="text" id="color-hexcode-input" name="hexcode"
+          value={colorForm.hexcode} onChange={change} />
+      </div>
+      <button type="button" onClick={submitColor}>{buttonText}</button>
+    </form>
+  );
+
+};
--- demo-app/src/components/ColorTool.js
+++ demo-app/src/components/ColorTool.js
@@ -2,25 +2,12 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import { ToolHeader } from './ToolHeader';
+import { ColorForm } from './ColorForm';
 
 export const ColorTool = ({ colors: initialColors }) => {
 
-  // array destructuring
-  const [ colorForm, setColorForm ] = useState({
-    name: '', hexcode: '',
-  });
-
   const [ colors, setColors ] = useState(initialColors.concat());
 
-  const change = (e) => {
-
-    setColorForm({
-      // object spread operator
-      ...colorForm, // copy the original color form
-      [ e.target.name ]: e.target.value,
-    });
-
-  };
 
   const addColor = () => {
     setColors(colors.concat({
@@ -36,20 +23,6 @@ export const ColorTool = ({ colors: initialColors }) => {
         {colors.map(color =>
           <li key={color.id}>{color.name}</li>)}
       </ul>
-      <form>
-        <div>
-          {/* React.createElement('label', { htmlFor: 'color-name-input' }, 'Color Name') */}
-          <label htmlFor="color-name-input">Color Name</label>
-          <input type="text" id="color-name-input" name="name"
-            value={colorForm.name} onChange={change} />
-        </div>
-        <div>
-          <label htmlFor="color-hexcode-input">Color Hexcode</label>
-          <input type="text" id="color-hexcode-input" name="hexcode"
-            value={colorForm.hexcode} onChange={change} />
-        </div>
-        <button type="button" onClick={addColor}>Add Color</button>
-      </form>
     </>
   );
 
